7e31ac41c6d8d9de2768c5e476a48666f0adaefc
Arvid Lindenau <alin@num42.de>
2021-12-14 22:14:04 +0100

    add basic stack thanks to onorio
    https://onor.io/2014/06/02/a-naive-stack-implementation-in-elixir-2/

--- lib/memory/stack.ex
+++ lib/memory/stack.ex
@@ -3,4 +3,21 @@ defmodule Esolix.Memory.Stack do
   Documentation for the simulated stack used by different esolangs.
   """
 
+  alias Esolix.Memory.Stack
+
+  defstruct elements: []
+
+  def init, do: %Stack{}
+
+  def push(stack, element) do
+    %Stack{stack | elements: [element | stack.elements]}
+  end
+
+  def pop(%Stack{elements: []}), do: raise "Stack is empty!"
+  def pop(%Stack{elements: [top | rest]}) do
+    {top, %Stack{elements: rest}}
+  end
+
+  def depth(%Stack{elements: elements}), do: length(elements)
+
 end
