1fc0d194be2f57dff192fb31e26c0bcaeb25f6a7
Arvid Lindenau <alin@num42.de>
2021-12-15 22:21:02 +0100

    add brainfuck mix task, add benchmarks

---
+++ lib/mix/brainfuck.ex
@@ -0,0 +1,21 @@
+defmodule Mix.Tasks.Brainfuck do
+  @moduledoc "Mix task for evaluating .bf-files or Brainfuck Code"
+  use Mix.Task
+
+  alias Esolix.Langs.Brainfuck
+
+  def run(args) do
+    case length(args) do
+      0 ->
+        IO.warn("No Argument provided to Mix Task.")
+      _ ->
+        arg = Enum.at(args, 0)
+        if File.exists?(Enum.at(args, 0)) do
+          Brainfuck.eval_file(arg)
+        else
+          Brainfuck.eval(arg)
+        end
+    end
+  end
+
+end
