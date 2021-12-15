22790cfb7d0b74b90ef9a7e89150c38a28714243
Arvid Lindenau <alin@num42.de>
2021-12-15 22:58:33 +0100

    add mix task template, rename folder

---
+++ lib/mix/template.ex
@@ -0,0 +1,21 @@
+defmodule Mix.Tasks.Template do
+  @moduledoc "Mix task for evaluating Template Code"
+  use Mix.Task
+
+  alias Esolix.Langs.Template
+
+  def run(args) do
+    case length(args) do
+      0 ->
+        IO.warn("No Argument provided to Mix Task.")
+      _ ->
+        arg = Enum.at(args, 0)
+        if File.exists?(Enum.at(args, 0)) do
+          Template.eval_file(arg)
+        else
+          Template.eval(arg)
+        end
+    end
+  end
+
+end
