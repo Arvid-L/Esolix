25558b85ecd1f04088edb16419b21db2bc8251e8
Arvid Lindenau <alin@num42.de>
2021-12-15 23:28:37 +0100

    add templates, add templating mix task for autogeneration

---
+++ lib/mix/gen_template.ex
@@ -0,0 +1,30 @@
+defmodule Mix.Tasks.Template.Gen do
+  @moduledoc "Mix task for evaluating Template Code"
+  use Mix.Task
+
+  @path_mix "#{File.cwd!}/lib/mix"
+  @path_lang "#{File.cwd!}/lib/langs"
+  @path_test "#{File.cwd!}/test/langs"
+
+  def run(args) do
+    case length(args) do
+      0 ->
+        IO.warn("No Argument provided to Mix Task.")
+      _ ->
+        arg = Enum.at(args, 0)
+
+        System.cmd("cp", ["#{@path_mix}/_template.ex", "#{@path_mix}/#{arg}.ex"])
+        System.cmd("gsed", ["-ri", "s/Template/#{String.capitalize(arg)}/", "#{@path_mix}/#{arg}.ex"])
+
+        System.cmd("cp", ["#{@path_lang}/_template.ex", "#{@path_lang}/#{arg}.ex"])
+        System.cmd("gsed", ["-ri", "s/Template/#{String.capitalize(arg)}/", "#{@path_lang}/#{arg}.ex"])
+
+        System.cmd("mkdir", ["#{@path_test}/#{arg}"])
+        System.cmd("cp", ["#{@path_test}/_template.ex", "#{@path_test}/#{arg}/#{arg}_test.ex"])
+        System.cmd("gsed", ["-ri", "s/Template/#{String.capitalize(arg)}/", "#{@path_test}/#{arg}/#{arg}_test.ex"])
+        System.cmd("gsed", ["-ri", "s/template/#{arg}/", "#{@path_test}/#{arg}/#{arg}_test.ex"])
+
+    end
+  end
+
+end
---
+++ test/langs/_template.ex
@@ -0,0 +1,22 @@
+defmodule Esolix.Langs.TemplateTest do
+  use ExUnit.Case, async: true
+  import ExUnit.CaptureIO
+
+  alias Esolix.Langs.Template
+
+  describe "eval/3" do
+    test "Example code should print \"Hello World!\"" do
+      assert capture_io(fn ->
+        Template.eval("some code")
+      end) == "Hello World!\n"
+    end
+
+  describe "eval_file/3" do
+    test "hello_world file should print \"Hello World!\"" do
+      assert capture_io(fn ->
+        Template.eval_file("test/langs/template/hello_world")
+      end) == "Hello World!\n"
+    end
+  end
+
+end
