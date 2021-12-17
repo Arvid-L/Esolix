fd8633b4d70422445a196c6d68136b0323a9c670
Arvid Lindenau <alin@num42.de>
2021-12-17 15:16:58 +0100

    add some chicken stuff

--- lib/langs/chicken.ex
+++ lib/langs/chicken.ex
@@ -4,17 +4,24 @@ defmodule Esolix.Langs.Chicken do
   """
 
   # Data Structure used:
-  # alias Esolix.DataStructures.Tape
+  alias Esolix.DataStructures.Stack
+
+  # 0	exit      axe	      Stop execution.
+  # 1	chicken	  chicken	  Push the string "chicken" onto the stack.
+  # 2	add	      add	      Add two top stack values.
+  # 3	subtract  fox	      Subtract two top stack values.
+  # 4	multiply	rooster	  Multiply two top stack values.
+  # 5	compare	  compare	  Compare two top stack values for equality, push truthy or falsy result onto the stack.
+  # 6	load	    pick	    Double wide instruction. Next instruction indicates source to load from. 0 loads from stack, 1 loads from user input. Top of stack points to address/index to load onto stack.
+  # 7	store	    peck	    Top of stack points to address/index to store to. The value below that will be popped and stored.
+  # 8	jump	    fr	      Top of stack is a relative offset to jump to. The value below that is the condition. Jump only happens if condition is truthy.
+  # 9	char	    BBQ	      Interprets the top of the stack as ascii and pushes the corresponding character.
+  # 10+	        push		  Pushes the literal number n-10 onto the stack.
 
 
   # Custom Module Errors
-  defmodule CustomModuleError do
-    defexception [:message]
-
-    def exception() do
-      msg = "Something went wrong I think"
-      %CustomModuleError{message: msg}
-    end
+  defmodule IntruderInChickenCoopError do
+    defexception message: "Invalid entry in chicken source code: Only \"chicken\" and \"\\n\" allowed."
   end
 
 
@@ -57,8 +64,13 @@ defmodule Esolix.Langs.Chicken do
   end
 
   defp validate_code(code) do
-    graphemes = String.graphemes(code)
-    # Do something
+    String.split(code, " ")
+    |> Enum.any?( & &1 != "chicken" )
+    |> if do
+      raise IntruderInChickenCoopError
+    end
+
+    code
   end
 
 end
